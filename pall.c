7f81e9f9c677ab883963605b3a5345545fc758ab
jaymfune <jacobmfune2@gmail.com>
2023-12-14 19:05:12 +0200

    fix errors

--- pall.c
+++ pall.c
@@ -9,7 +9,7 @@ void pall(stack_t **stack, unsigned int line_number)
 {
     stack_t *current = *stack;
 
-    (void)line_number; // Unused parameter
+    (void)line_number; /* Unused parameter */
 
     while (current != NULL)
     {
