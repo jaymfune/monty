4e408b6678a1b958442fb2b207f2ba6d519b7dd3
jaymfune <jacobmfune2@gmail.com>
2023-12-14 09:36:50 -0800

    betty fixes

--- push.c
+++ push.c
@@ -8,29 +8,28 @@
  */
 void push(stack_t **stack, int value, unsigned int line_number)
 {
-    stack_t *new_node;
+	stack_t *new_node;
+	(void)line_number;
+	
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	new_node->n = value;
+	new_node->prev = NULL;
 
-    (void)line_number;
-
-    new_node = malloc(sizeof(stack_t));
-    if (new_node == NULL)
-    {
-        fprintf(stderr, "Error: malloc failed\n");
-        exit(EXIT_FAILURE);
-    }
-
-    new_node->n = value;
-    new_node->prev = NULL;
-
-    if (*stack == NULL)
-    {
-        new_node->next = NULL;
-    }
-    else
-    {
-        new_node->next = *stack;
-        (*stack)->prev = new_node;
-    }
-
-    *stack = new_node;
+	if (*stack == NULL)
+	{
+		new_node->next = NULL;
+	}
+	else
+	{
+		new_node->next = *stack;
+		(*stack)->prev = new_node;
+	}
+	
+	*stack = new_node;
 }
