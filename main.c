d620be726ed36caae05ae38e7c8e9f4b4d4b0d1a
jaymfune <jacobmfune2@gmail.com>
2023-12-14 09:46:39 -0800

    swap and add

--- main.c
+++ main.c
@@ -5,26 +5,15 @@ int main(void)
     stack_t *stack = NULL;
     unsigned int line_number = 1;
 
-    /* Test push with valid integer */
     push(&stack, 10, line_number);
-    pall(&stack, line_number);
-
-    /* Test pint on non-empty stack */
-    pint(&stack, line_number);
-
-    /* Test pop on non-empty stack */
-    pop(&stack, line_number);
-    pall(&stack, line_number);
-
-    /* Test pop on empty stack */
-    pop(&stack, line_number);
-
-    /* Test push with invalid input */
     push(&stack, 20, line_number);
+    push(&stack, 30, line_number);
+
+    swap(&stack, line_number);
     pall(&stack, line_number);
 
-    /* Test pall with non-empty stack */
-    pall(&stack, line_number);
+    pop(&stack, line_number);
+    swap(&stack, line_number);
 
     /* Clean up the stack (free memory) */
     while (stack != NULL)
---
+++ swap.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+
+/**
+ * swap - swaps the top two elements of the stack
+ * @stack: double pointer to the beginning of the stack
+ * @line_number: line number in the Monty byte code file
+ */
+void swap(stack_t **stack, unsigned int line_number)
+{
+	int temp;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = (*stack)->n;
+	(*stack)->n = (*stack)->next->n;
+	(*stack)->next->n = temp;
+}
