1b6c7393cd424598de49c795702d014424cdecab
jaymfune <jacobmfune2@gmail.com>
2023-12-14 19:11:37 +0200

    fix errors

--- main.c
+++ main.c
@@ -1,3 +1,5 @@
+// main.c
+
 #include "monty.h"
 
 int main(void)
@@ -6,17 +8,14 @@ int main(void)
     unsigned int line_number = 1;
 
     /* Test push with valid integer */
-    instruction.opcode = "push";
     push(&stack, 10, line_number);
     pall(&stack, line_number);
 
     /* Test push with invalid input */
-    instruction.opcode = "push";
     push(&stack, 20, line_number);
     pall(&stack, line_number);
 
     /* Test pall with non-empty stack */
-    instruction.opcode = "pall";
     pall(&stack, line_number);
 
     /* Clean up the stack (free memory) */
