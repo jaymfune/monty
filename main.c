dbd615dff2a1c3d72b71923ce77cc1a8543fb187
jaymfune <jacobmfune2@gmail.com>
2023-12-16 06:05:11 -0800

    fixes

--- main.c
+++ main.c
@@ -4,15 +4,16 @@ int main(void)
 {
 	stack_t *stack = NULL;
 	unsigned int line_number = 1;
-	
+
 	push(&stack, 10, line_number);
 	push(&stack, 20, line_number);
 	push(&stack, 0, line_number);
-	
+
 	/* Clean up the stack (free memory) */
 	while (stack != NULL)
 	{
 		stack_t *temp = stack->next;
+
 		free(stack);
 		stack = temp;
 	}
--- pall.c
+++ pall.c
@@ -8,8 +8,7 @@
 void pall(stack_t **stack, unsigned int line_number)
 {
 	stack_t *current = *stack;
-	(void)line_number; /* Unused parameter */
-	
+	(void)line_number;/* Unused parameter */
 	while (current != NULL)
 	{
 		printf("%d\n", current->n);
